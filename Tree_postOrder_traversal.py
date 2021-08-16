39e2c76f49b8d22a10dd5149f00e60d90f9ff7b6
AVANISH SINGH <68435409+avanishsingh07@users.noreply.github.com>
2021-08-16 09:29:42 +0530

    Update Tree_postOrder_traversal.py

--- Tree_postOrder_traversal.py
+++ Tree_postOrder_traversal.py
@@ -1,9 +1,9 @@
 
-Node is defined as
-self.left (the left child of the node)
-self.right (the right child of the node)
-self.info (the value of the node)
-"""
+#Node is defined as
+#self.left (the left child of the node)
+#self.right (the right child of the node)
+#self.info (the value of the node)
+
 def postOrder(root):
     #Write your code here
     if root == None:
